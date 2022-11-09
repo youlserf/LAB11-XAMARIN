ae556d3ea0126571c36a341fc6eb7474d216aab5
Youlserf <youlserf.cardenas@tecsup.edu.pe>
2022-11-09 15:48:26 -0500

    complete lab, I couldn't add display alert

---
+++ MVVMDemo/MVVMDemo/Models/ModelConstantes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVMDemo.Models
+{
+    public static class ModelConstantes
+    {
+        public static double Pi = 3.14;
+    }
+}
---
+++ MVVMDemo/MVVMDemo/ViewModels/ViewModelOperaciones.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVMDemo.ViewModels
+{
+    public class ViewModelOperaciones :ViewModelBase
+    {
+
+        #region  Atributos
+        string message;
+        public string Message
+        {
+            get { return message; }
+            set
+            {
+                if (message != value)
+                {
+                    message = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        int valorA;
+        public int ValorA
+        {
+            get { return valorA; }
+            set
+            {
+                if (valorA != value)
+                {
+                    valorA = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int valorB;
+        public int ValorB
+        {
+            get { return valorB; }
+            set
+            {
+                if (valorB != value)
+                {
+                    valorB = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int resultadoAll;
+        public int Resultado
+        {
+            get { return resultadoAll; }
+            set
+            {
+                if (resultadoAll != value)
+                {
+                    resultadoAll = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        public ICommand Sumar { protected set; get; }
+        public ICommand Restar { protected set; get; }
+        public ICommand Dividir { protected set; get; }
+        public ICommand Multiplicar { protected set; get; }
+
+
+        public ViewModelOperaciones()
+        {
+            Sumar = new Command(() =>
+            {
+                Message = "El resultado de la suma es:";
+                Resultado = ValorA + ValorB;
+            });
+
+            Restar = new Command(() =>
+            {
+                Message = "El resultado de la resta es:";
+                Resultado = ValorA - ValorB;
+            });
+
+            Multiplicar = new Command(() =>
+            {
+                Message = "El resultado de la multiplicación es:";
+                Resultado = ValorA * ValorB;
+
+            });
+
+            Dividir = new Command(() =>
+            {
+                if (ValorB == 0)
+                {
+                    Resultado = 0;
+                    Message = "No puedes dividir entre 0";
+                    return;
+                }
+                else {
+                    Message = "El resultado de la división es:";
+                    Resultado = ValorA / ValorB;
+                }
+                
+
+            });
+
+        }
+
+    }
+}
